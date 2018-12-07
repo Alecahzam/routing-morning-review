c760e7a75be8311b878324a41c4204e34ab80660
Tim Biles <timbilestimbiles@gmail.com>
2018-12-07 10:37:31 -0600

    edit readme, clean-up

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 - Routing from component to component
 - Utilizing this.props.match.params
 - Using a json file as a fake database
+
+## Flow
+- Start with the setup in App.js
+- Then go to the routes.js file
+- Discuss how the Link tag works in home.js
--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 // import all routes together. Remember to wrap with a Router (I aliased it from BrowserRouter, HashRouter works as well)
 import routes from './routes';
-import Header from './Components/Header/Header'
+import Header from './Components/Header/Header';
 
 class App extends Component {
   render() {
     return (
-      <Router >
-      <div className='App'>
-        < Header />
-        {routes}
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          {routes}
+        </div>
       </Router>
     );
   }
